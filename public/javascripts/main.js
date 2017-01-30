c4638787089aa24127fdd155c4dfcb4b5079c7a0
ginotria <romanangelo.tria@gmail.com>
2017-01-31 04:52:50 +0800

    * dynamic page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,3 +15,20 @@ $('#delete').on('click', function(e){
 
   });
 });
+
+
+
+if (window.location.pathname === '/contacts') {
+
+  fetch('api/v1/Contact').then(function(res) {
+    res.json().then(function(contacts) {
+      console.log('contacts', contacts);
+      var tbody = document.getElementById('table-body');
+      contacts.forEach(function(contact) {
+        tbody.insertAdjacentHTML('beforeend', '<tr> <td>  <input type="checkbox" id="' + contact._id + '" />  </td>  <td>  <a href="/contacts/#' + contact._id + '">' + contact.name + '</a></td> <td> ' + contact.nickname + '</td> <td>' + contact.email + ' </td> </tr>');
+
+      });
+    })
+  });
+
+}
